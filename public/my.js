18a71ca3a9b2c670543b56d21a23b3f3bdd21662
bogdan8 <bogdanlitvin8@gmail.com>
2016-04-24 14:50:34 +0300

    said counter and buttons for adding and subtracting product and cleaning basket

--- public/my.js
+++ public/my.js
@@ -1,18 +1,17 @@
-function sumfing(){
-	var x = window.localStorage.getItem('b');
-
-	x = x *1 + 1;
-
-	window.localStorage.setItem('b' , x);
-
-	alert(x);
-}
 function add_to_cart(id){
 	var name = 'product_' + id;
 	var x = window.localStorage.getItem(name);
 	x = x * 1 + 1;
 	window.localStorage.setItem(name , x);
+	document.getElementById("count" + id).innerHTML = x;
+}
 
+function delete_to_cart(id){
+	var name = 'product_' + id;
+	var x = window.localStorage.getItem(name);
+	x = x * 1 - 1;
+	window.localStorage.setItem(name , x);
+	document.getElementById("count" + id).innerHTML = x;
 }
 
 function clear_local(){

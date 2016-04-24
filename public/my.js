b58b73d6443fa15599fba855f25c25596397e532
bogdan8 <bogdanlitvin8@gmail.com>
2016-04-24 14:38:16 +0300

    finaly add_to_cart

--- public/my.js
+++ public/my.js
@@ -8,5 +8,14 @@ function sumfing(){
 	alert(x);
 }
 function add_to_cart(id){
-	alert('you anded pizza with id:' + id);
+	var name = 'product_' + id;
+	var x = window.localStorage.getItem(name);
+	x = x * 1 + 1;
+	window.localStorage.setItem(name , x);
+
+}
+
+function clear_local(){
+	window.localStorage.clear();
+	alert('Ваша корзина очищена');
 }

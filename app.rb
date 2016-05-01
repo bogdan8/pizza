8753863223a7ab7b23adbfea981d5a3e0a511b24
bogdan8 <bogdanlitvin8@gmail.com>
2016-05-01 12:06:27 +0300

    change linewraper and add js function for count pizza

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
 ActiveRecord::Base.establish_connection(
-  :adapter  => "mysql2",
-  :host     => "localhost",
+  :adapter => "mysql2",
+  :host => "localhost",
   :username => "root",
   :password => "70sunogo",
   :database => "pizza"
@@ -19,52 +19,53 @@ class Order < ActiveRecord::Base
 end
 
 get '/' do
-	@product = Product.all
-	erb :index	
+  @product = Product.all
+  erb :index
 end
 
 get '/about' do
-	erb :about
+  erb :about
 end
 
 post '/cart' do
-	orders_input = params[:orders]
-	@items = perse_orders_input orders_input
+  orders_input = params[:orders]
+  @items = perse_orders_input orders_input
 
-	@items.each do |item|
-		item[0] = Product.find(item[0])
-	end
+  @items.each do |item|
+    item[0] = Product.find(item[0])
+  end
 
-	erb :cart
+  erb :cart
 end
 
-post '/place_order' do 
-	@order = Order.create params[:order]
-	
-	erb :order_placed
+post '/place_order' do
+  @order = Order.create params[:order]
+
+  erb :order_placed
 end
 
-get '/admin_new' do 
-	erb :admin_new
+get '/admin_new' do
+  erb :admin_new
 end
 
 post '/admin_new' do
-	@product = Product.create params[:product]
-	erb "Дякуєм за замовлення"
+  @product = Product.create params[:product]
+
+  erb "Дякуєм за замовлення"
 end
 
 def perse_orders_input orders_input
-	s1 = orders_input.split(/,/)
-	arr = []
+  s1 = orders_input.split(/,/)
+  arr = []
 
-	s1.each do |x|
-		s2 = x.split(/\=/)
-		s3 = s2[0].split(/_/)
-		id = s3[1]
-		cnt = s2[1]
-		arr2 = [id,cnt]
-		arr.push arr2
-	end
+  s1.each do |x|
+    s2 = x.split(/\=/)
+    s3 = s2[0].split(/_/)
+    id = s3[1]
+    cnt = s2[1]
+    arr2 = [id, cnt]
+    arr.push arr2
+  end
 
-	return arr
+  return arr
 end
--- public/my.js
+++ public/my.js
@@ -1,66 +1,65 @@
-function add_to_cart(id){
-	var name = 'product_' + id;
-	var x = window.localStorage.getItem(name);
-	x = x * 1 + 1;
-	window.localStorage.setItem(name , x);
-	document.getElementById("count" + id).innerHTML = x;
-	document.getElementById("count").innerHTML = get_count_items();
-	update_orders();
+function add_to_cart(id) {
+    var name = 'product_' + id;
+    var x = window.localStorage.getItem(name);
+    x = x * 1 + 1;
+    window.localStorage.setItem(name, x);
+    document.getElementById("count" + id).innerHTML = x;
+    document.getElementById("count").innerHTML = get_count_items();
+    update_orders();
 }
 
-function delete_to_cart(id){
-	var name = 'product_' + id;
-	var x = window.localStorage.getItem(name);
-	console.log(x);
-	if(x == '-1'){
-		alert("Ви недодали піцу");
-		window.localStorage.clear();
-		location.reload();
-	}else{
-		x = x * 1 - 1;
-		window.localStorage.setItem(name , x);
-		document.getElementById("count" + id).innerHTML = x;
-		document.getElementById("count").innerHTML = get_count_items();
-		update_orders();
-	}
+function delete_to_cart(id) {
+    var name = 'product_' + id;
+    var x = window.localStorage.getItem(name);
+    console.log(x);
+    if (x == '-1') {
+        alert("Ви недодали піцу");
+        window.localStorage.clear();
+        location.reload();
+    } else {
+        x = x * 1 - 1;
+        window.localStorage.setItem(name, x);
+        document.getElementById("count" + id).innerHTML = x;
+        document.getElementById("count").innerHTML = get_count_items();
+        update_orders();
+    }
 }
 
-function clear_local(){
-	window.localStorage.clear();
-	location.reload();
-	alert('Ваша корзина очищена');
+function clear_local() {
+    window.localStorage.clear();
+    location.reload();
+    alert('Ваша корзина очищена');
 }
 
-function get_count_items(){
-	var cnt = 0;
-	for (var i = 0; i < window.localStorage.length; i++) {
-		var key = window.localStorage.key(i);//ключ
-		var value = window.localStorage.getItem(key);//значення
+function get_count_items() {
+    var cnt = 0;
+    for (var i = 0; i < window.localStorage.length; i++) {
+        var key = window.localStorage.key(i);//ключ
+        var value = window.localStorage.getItem(key);//значення
 
-		if(key.indexOf('product_') == 0){
-			cnt = cnt + value * 1;
-		}
-	}
-	return cnt;
+        if (key.indexOf('product_') == 0) {
+            cnt = cnt + value * 1;
+        }
+    }
+    return cnt;
 }
 
-function update_orders(){
-	var orders = get_orders();
-	console.log(orders);
-	document.getElementById('orders_input').value = orders;
+function update_orders() {
+    var orders = get_orders();
+    console.log(orders);
+    document.getElementById('orders_input').value = orders;
 }
 
 
+function get_orders() {
+    var orders = '';
+    for (var i = 0; i < window.localStorage.length; i++) {
+        var key = window.localStorage.key(i);//ключ
+        var value = window.localStorage.getItem(key);//значення
 
-function get_orders(){
-	var orders = '';
-	for (var i = 0; i < window.localStorage.length; i++) {
-		var key = window.localStorage.key(i);//ключ
-		var value = window.localStorage.getItem(key);//значення
-
-		if(key.indexOf('product_') == 0){
-			orders = orders + key + '=' + value + ',';
-		}
-	}
-	return orders;
+        if (key.indexOf('product_') == 0) {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+    return orders;
 }
